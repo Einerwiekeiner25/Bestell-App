7cea9b2c5d7e8487ebda493f4349354b779242e2
Yannick Becker <beckeryannick18@gmail.com>
2025-05-13 12:07:17 +0200

    add template.js, coder downsized, change font-size, delete db.js, change responsiv, add 2 icons

--- script.js
+++ script.js
@@ -12,26 +12,12 @@ function addToCart(name) {
     }
 }
 
-function increaseDish(name) {
+function changeDishAmount(name, plusorminus) {
     for (let category in menu) {
         let dishes = menu[category];
         for (let dish of dishes) {
             if (dish.name === name) {
-                dish.amount++;
-                renderCart();
-                updateCartCount();
-                return;
-            }
-        }
-    }
-}
-
-function reduceDish(name) {
-    for (let category in menu) {
-        let dishes = menu[category];
-        for (let dish of dishes) {
-            if (dish.name === name) {
-                dish.amount--;
+                dish.amount += plusorminus;
                 renderCart();
                 updateCartCount();
                 return;
@@ -51,10 +37,39 @@ function updateCartCount() {
     document.getElementById('counts-cart').innerText = total;
 }
 
-
 function toggleCartResponsiv() {
     let cartRef = document.getElementById("shopping-cart-responsiv")
     cartRef.classList.toggle("d-none")
+}
+
+function buildCartContent() {
+    let cartContentHTML = '';
+    let subTotal = 0;
+
+    for (let category in menu) {
+        for (let dish of menu[category]) {
+            if (dish.amount) {
+                let priceNumber = parseFloat(dish.price.replace(',', '.').replace('€', ''));
+                let dishTotal = dish.amount * priceNumber;
+                subTotal += dishTotal;
+
+                cartContentHTML += shoppingCartItem(dish, dishTotal);
+            }
+        }
+    }
+
+    return {cartContentHTML, subTotal};
+}
+
+function filledShoppingCart(){
+    for (let category in menu) {
+        for (let dish of menu[category]) {
+            if (dish.amount > 0) {
+                return true;
+            }
+        }
+    }
+    testCheckoutHtml();
 }
 
 document.getElementById("cart-header-responsiv-section").addEventListener("click", function(event) {
--- script/html_script.js
+++ script/html_script.js
@@ -1,5 +1,3 @@
-
-
 let mainRef = document.getElementById("main");
 let cartContentRef = document.getElementById("cart-content");
 let cartContentResponsivRef = document.getElementById("cart-content-responsiv")
@@ -7,119 +5,26 @@ let countsCartRef = document.getElementById("counts-cart")
 const deliverFee = 3.50;
 
 function renderMain() {
-    let mainHtml = '';
+    let html = '';
 
-    mainHtml += `        
-    <div class="restaurant-details">
-            <div class="restaurant-info">
-                <img src="assets/Restaurant_logo/Ristorante Bella Italia.png" alt="Ristorante Bella Italia">
-                <h1>Ristorante Bella Italia</h1>
-                <div class="restaurant-info-details">
-                    <div>
-                        <span class="star">★ 4.7</span>
-                        <span>(324)</span>
-                        <span>Italienisch</span>
-                    </div>
-                    <div>
-                        <span>🚚</span>
-                        <span>Lieferung: 3,50 €</span>
-                    </div>
-                    <div>
-                        <span>⏱️</span>
-                        <span>25-35 Min.</span>
-                    </div>
-                </div>   
-            </div>
-        </div> 
-        <div class="opening-hours">
-            <span>Dieses Restaurant hat 24 Stunden geöffnet.</span>
-        </div>
-
-        <div class="categories" id="categories">
-            <h3>Kategorien</h3>
-            <div class="category-list">
-                <a href="#Angebote">Angebote</a>
-                <a href="#Vorspeisen">Vorspeisen</a>
-                <a href="#Pizza">Pizza</a>
-                <a href="#Pasta">Pasta</a>
-                <a href="#Salate">Salate</a>
-                <a href="#Desserts">Desserts</a>
-                <a href="#Getränke">Getränke</a>
-            </div>
-        </div>`;
-
+    html += restaurantDetailsHtml()
     for (let category in menu) {
-        mainHtml += `
-                <div class="category-section" id="${category}">
-                    <h2 class="category-title">${category}</h2>
-                    <div class="items-flex">
-            `;
+        html += categorySectionHtml(category)
 
         let dishes = menu[category];
         for (let dish of dishes) {
-            mainHtml += `
-                    <div class="menu-item">
-                        <div class="item-image">
-                            <img src="${dish.pictures}" alt="${dish.name}">
-                        </div>
-                        <div class="item-details">
-                            <div class="item-name">${dish.name}</div>
-                            <div class="item-description">${dish.description}</div>
-                            <div class="item-price">
-                                <span>${dish.price}</span>
-                                <button onclick="addToCart('${dish.name}','${dish.price}')" class="add-button">+</button>
-                            </div>
-                        </div>
-                    </div>
-                `;
+            html += menuItemsHtml(dish)
         }
 
-        mainHtml += `
-                    </div>
-                </div>
-                `;
+        html += divForMainHtml()
     }
 
-    mainHtml += `
-            <div class="background-shopping-cart">          
-                <div class="cart-button" id="cart-button" onclick="toggleCartResponsiv()">
-                    <span>Warenkorb</span>
-                    <span id="counts-cart" onload="renderResponsivCart()"> 0 </span>
-                </div>
-            </div>`
-                
-
-    mainRef.innerHTML = mainHtml;
+    html += backgroundShoppingCartHtml()            
+    mainRef.innerHTML = html;
 }
 
 function renderCart() {
-    let cartContentHTML = '';
-    let subTotal = 0;
-
-
-    for (let category in menu) {
-        for (let dish of menu[category]) {
-            if (dish.amount) {
-                let priceNumber = parseFloat(dish.price.replace(',', '.').replace('€', ''));
-                let dishTotal = dish.amount * priceNumber;
-                subTotal += dishTotal;
-
-                cartContentHTML += `
-                <div class="shopping-cart-item">
-                    <div class="dish-quantity">${dish.amount}x</div>
-                    <div class="dish-info">
-                        <div>${dish.name}</div>
-                        <div>${dishTotal.toFixed(2)} €</div>
-                    </div>
-                    <div class="shopping-cart-button">
-                        <button class="quantity-button" onclick="reduceDish('${dish.name}')">-</button>
-                        <button class="quantity-button" onclick="increaseDish('${dish.name}')">+</button>
-                    </div>
-                </div>
-                `;
-            }
-        }
-    }
+    let {cartContentHTML, subTotal} = buildCartContent();
 
     cartContentRef.innerHTML = cartContentHTML;
     cartContentResponsivRef.innerHTML = cartContentHTML;
@@ -130,25 +35,11 @@ function renderCart() {
 }
 
 function pressCheckout() {
-    let filledShoppingCart = false;
-    for (let category in menu) {
-        for (let dish of menu[category]) {
-            if (dish.amount > 0) {
-                filledShoppingCart = true;
-                break;
-            }
-        }
-        if (filledShoppingCart) break;
-    }
-
-    if (filledShoppingCart === false) return;
-
+    if (!filledShoppingCart()) return;
+    
     let cartContentHTML = '';
 
-    cartContentHTML += `
-                <div
-                <p class="test-checkout">Testbestellung wurde vorgenommen!</p>
-                </div>`
+    cartContentHTML += testCheckoutHtml()
     
     for (let category in menu) {
         for (let dish of menu[category]) {
@@ -156,10 +47,7 @@ function pressCheckout() {
         }
     }
     renderCart();
+    updateCartCount();
     cartContentRef.innerHTML = cartContentHTML;
     cartContentResponsivRef.innerHTML = cartContentHTML;
-
-    setTimeout(() => {
-        renderCart();
-    }, 10000);
 } 
